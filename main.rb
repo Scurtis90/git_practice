cd80f254c9c01f21c4c41526027648fed0e18079
Spencer Curtis <spencerscurtis@gmail.com>
2021-02-24 19:06:05 -0700

    create menu and git helper and colorize

---
+++ main.rb
@@ -0,0 +1,27 @@
+require 'colorize'
+
+def puts_git_info(cmd)
+  puts 'Your command:'
+  puts 'git #{cmd} -h'
+  menu
+end
+
+
+def menu
+  puts 'Main Menu'.colorize(:cyan)
+  puts '1. Enter git command'.colorize(:green)
+  puts '2. Exit'.colorize(:blue)
+  choice = gets.strip.to_i
+  case choice
+  when 1
+    puts 'Enter git command'.colorize(:yellow)
+    puts_git_info(gets.strip)
+  when 2
+    exit
+  else
+    'Invalid choice, try 1 or 2'.colorize(:error)
+    menu
+  end
+end
+
+menu
